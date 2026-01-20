7710bc349dded67c69c85afcf7bd1f22abd572d6
canaliego08 <56915097+canaliego08@users.noreply.github.com>
2026-01-20 13:57:47 +0100

    Change environment variable for database password

--- public/admin.php
+++ public/admin.php
@@ -96,7 +96,7 @@ endif;
 $host = getenv("DB_HOST");
 $dbname = getenv("DB_NAME");
 $user = getenv("DB_USER");
-$password = getenv("DB_PASSWORD");
+$password = getenv("DB_PASS");
 
 try {
     $pdo = new PDO(
